fe8cc7f469166b5772bb7ff3f460871215a3227b
Jaziel <jaziel.isma@outlook.es>
2021-03-22 11:57:00 -0300

    practia de hook basica finalizada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react';
+import Itachi from './Components/Itachi';
+import Minato from './Components/Minato';
+import './styles/App.css';
 
 function App() {
+
+  const [character, setCharacter] = useState(true);
+
+  const toggleCharacter = () => {
+    if(character){
+      setCharacter(false);
+    } else {
+      setCharacter(true)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {character ? <Itachi toggleCharacter={toggleCharacter}/> : <Minato toggleCharacter={toggleCharacter}/>}
+    </Fragment>
   );
 }
 
